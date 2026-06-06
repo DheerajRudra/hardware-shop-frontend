1d61107341178563d315947c5e6f950089491cc6
DheerajRudra <dheeraj2023rudra@gmail.com>
2026-06-06 11:55:40 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,24 +1,22 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'https://hardware-shop-backend-avxa.onrender.com/api'
-});
+const API = axios.create({ baseURL: '/api' });
 
-export const getProducts        = ()        => API.get('/products');
-export const getDeletedProducts = ()        => API.get('/products/deleted');
-export const addProduct         = (data)    => API.post('/products', data);
-export const updateProduct      = (id,data) => API.put(`/products/${id}`, data);
-export const deleteProduct      = (id)      => API.delete(`/products/${id}`);
-export const restoreProduct     = (id)      => API.put(`/products/${id}/restore`);
+export const getProducts   = ()          => API.get('/products');
+export const addProduct    = (data)      => API.post('/products', data);
+export const updateProduct = (id, data)  => API.put(`/products/${id}`, data);
+export const deleteProduct = (id)        => API.delete(`/products/${id}`);
 
-export const getCustomers       = ()        => API.get('/customers');
-export const addCustomer        = (data)    => API.post('/customers', data);
-export const deleteCustomer     = (id)      => API.delete(`/customers/${id}`);
+export const getCustomers  = ()          => API.get('/customers');
+export const addCustomer   = (data)      => API.post('/customers', data);
+export const deleteCustomer= (id)        => API.delete(`/customers/${id}`);
+export const addPurchase   = (id, data)  => API.post(`/customers/${id}/purchases`, data);
+export const deletePurchase= (id, pid)   => API.delete(`/customers/${id}/purchases/${pid}`);
 
-export const getVendors         = ()        => API.get('/vendors');
-export const addVendor          = (data)    => API.post('/vendors', data);
-export const deleteVendor       = (id)      => API.delete(`/vendors/${id}`);
+export const getVendors    = ()          => API.get('/vendors');
+export const addVendor     = (data)      => API.post('/vendors', data);
+export const deleteVendor  = (id)        => API.delete(`/vendors/${id}`);
 
-export const getBills           = ()        => API.get('/bills');
-export const addBill            = (data)    => API.post('/bills', data);
-export const deleteBill         = (id)      => API.delete(`/bills/${id}`);
+export const getBills      = ()          => API.get('/bills');
+export const addBill       = (data)      => API.post('/bills', data);
+export const deleteBill    = (id)        => API.delete(`/bills/${id}`);
